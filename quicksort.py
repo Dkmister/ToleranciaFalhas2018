41f0e1c464ec5a3e9afa40bd8d104548fe2787e6
Vilmar Neto <vilmar.neto@gmail.com>
2018-04-08 18:13:13 -0300

    Adicionado quicksort feito por Fernando Garcia Bock

---
+++ quicksort.py
@@ -0,0 +1,45 @@
+def a_swap(array = [], li = 0, ri = 0):
+    aux = array[li]
+    array[li] = array[ri]
+    array[ri] = aux
+
+def partition(array, begin, end):
+    pivot = array[begin];
+    left_index = begin + 1;
+    right_index = end;
+    done = False
+    while not done:
+        while left_index <= right_index and array[left_index] < pivot:
+            left_index += 1
+        while right_index >= left_index and array[right_index] >= pivot:
+            right_index -= 1
+        if left_index > right_index:
+            done = True
+        else:
+            a_swap(array, left_index, right_index)
+    a_swap(array, begin, right_index)
+    return right_index
+
+def quicksort(array = [], begin = 0, end = 0):
+    if begin < end:
+        split_at = partition(array, begin, end)
+        quicksort(array, begin, split_at)
+        quicksort(array, split_at + 1, end)
+
+
+
+with open('input.txt', 'r') as file:
+    array = [];
+    substr = ' '
+    line = file.readline()
+    for i in line:
+        if (i != ' '):
+            substr += i
+        else:
+            array.append(int(substr)); substr = ' '
+    array.append(int(substr));
+    print('Initial Array:\t', array)
+    quicksort(array, 0, len(array) - 1)
+    print('Sorted Array:\t', array)
+
+
