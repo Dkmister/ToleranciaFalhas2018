de4fadc07adb658da5397df3c5d36ea20bea4a75
Vilmar Neto <vilmar.neto@gmail.com>
2018-04-13 18:37:18 +0000

    documentation for producerconsumer.py

--- producerconsumer.py
+++ producerconsumer.py
@@ -9,7 +9,16 @@ import random
 queue = []
 MAX_NUM = 10
 condition = Condition()
+"""
+classe Produtor :
 
+metodo run:
+
+Produz um item e coloca numa fila..
+
+Utiliza condition para fazer sincronização e espera da outra thread Consumidor.
+
+"""
 class ProducerThread(Thread):
 	def run(self):
 		nums = range(5)
@@ -27,7 +36,16 @@ class ProducerThread(Thread):
 			condition.notify()
 			condition.release()
 			time.sleep(random.random())
-			
+"""
+classe Consumidor:
+
+metodo run:
+
+Espera um item estar na fila construída pelo Produtor.
+
+Utiliza sincronização e espera , através do condition para a correta utilização do algoritmo.
+
+"""
 class ConsumerThread(Thread):
 	def run(self):
 		global queue
