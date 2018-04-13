0851311b662cb20460e72482fc936f2564f8dcd8
Vilmar Neto <vilmar.neto@gmail.com>
2018-04-13 18:32:25 +0000

    Documented multmat.py

--- multmat.py
+++ multmat.py
@@ -1,5 +1,23 @@
 # Codigo referente a multiplicacao entre duas matrizes
 # Feito por Vilmar Neto
+
+"""
+function:
+matrix result:= mat_mult (matrix a, matrix b)
+
+Given 2 matrixes that can be multiplied, returns the result of the matrix multiplication.
+
+It does the sum of the multiplication of each element in their place.
+
+"""
+
+
+def mat_mult(x,y):
+	result = [[sum(a*b for a,b in zip(x_row,y_col)) for y_col in zip(*y)] for x_row in x]
+	return result
+
+
+
 x = [[2,4],
      [4,8]]
 
@@ -7,7 +25,6 @@ x = [[2,4],
 y = [[1,0],
      [0,1]]
 
-result = [[sum(a*b for a,b in zip(x_row,y_col)) for y_col in zip(*y)] for x_row in x]
-
+result =  mat_mult(x,y)
 for r in result:
 	print(r)
